5d865f40f1a926293722201201c0df952d1e9e38
Pedro-IF <Pedro-IF@users.noreply.github.com>
2026-08-21 02:11:52 +0200

    fix: URL to main page in (website) server logs

--- server.js
+++ server.js
@@ -142,5 +142,5 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
+  console.log(`Server is running on http://localhost:${PORT}/pages/main.html`)
 })
